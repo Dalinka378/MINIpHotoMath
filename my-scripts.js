1b78928ffe18157eb0800406796997802c373701
Dalinka378 <mbalanici@icloud.com>
2025-03-05 09:19:24 +0200

    am facut calculatorul functional

--- my-scripts.js
+++ my-scripts.js
@@ -31,9 +31,102 @@ function solveExpression() {
     alert("Funcția de import din galerie va fi implementată aici");
   }
   function addKeyboard(){
-    let equation=document.getElementById('mathInput');
-    let text =equation.innerText.toLowerCase().replace(/[^a-z]/g, '');
-    equation.innerText=text;
-    
-   
+    const calcButtons = document.getElementsByClassName("calc-buttons")[0]
+    calcButtons.style = "display: block;"
   }
+
+  let runningTotal=0;
+  let buffer="0";
+  let previousOperator;
+
+  const screen = document.querySelector('.screen');
+
+  function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+  switch(symbol){
+    case 'C':
+      buffer= '0';
+      runningTotal =0;
+      break;
+      case '=':
+        if(previousOperator ===  null){
+          return
+        }
+        flushOperation(parseInt(buffer));
+        previousOperator=null;
+        buffer=runningTotal;
+        runningTotal=0;
+        break;
+        case '←':
+          if(buffer.length ==1){
+            buffer= '0';
+          }else{
+            buffer.toString(0, buffer.length -1);
+          }
+          break;
+          case '+':
+            case '-':
+              case '×':
+                case '÷':
+                  case '√':
+              handleMath(symbol);
+              break;
+        }            
+      
+}  
+
+function handleMath(symbol){
+  if(buffer === '0'){
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if(runningTotal === 0){
+    runningTotal=intBuffer;
+  }else{
+    flushOperation(intBuffer);
+  }
+  previousOperator=symbol;
+  buffer='0';
+}
+
+function flushOperation(intBuffer){
+  if(previousOperator === '+'){
+    runningTotal += intBuffer;
+  }else if(previousOperator === '-'){
+    runningTotal -= intBuffer;
+  }else if(previousOperator === '×'){
+    runningTotal*=intBuffer;
+  }else if(previousOperator === '÷'){
+    runningTotal /= intBuffer;
+  }else if(previousOperator === '√'){
+    runningTotal = Math.sqrt(intBuffer);
+  }
+}
+
+function handleNumber(handleNumber){
+  if(buffer === "0"){
+    buffer = handleNumber;
+  }else{
+    buffer += handleNumber;
+  }
+}
+
+function init(){
+  document.querySelector('.calc-buttons').
+  addEventListener ('click', function(event){
+    buttonClick(event.target.innerText);
+  })
+}
+
+
+init();
